b9be42f45a9d5450eba940cefe0e36aa98c08994
80010-09-04 <80010-09-04@exchange.ad.afpanet>
2020-02-27 11:11:34 +0100

    Ajout du dossier bootstrap
    Ajout du dossier boostrap et fin de l'exercice JS 04

---
+++ Algo&Javascript/Exo.js
@@ -0,0 +1,58 @@
+
+        // Demande le nom de famille
+
+// var MyVar; // Le type de la variable myVar est undefined
+// myVar = 324; // Type number
+// myVar = "Bonjour"; // type string
+// myVar = true; // type boolean
+// myVar = new Array(); // type object
+
+// console.log(typeof 42); // Affiche "number"
+// console.log(typeof 'blubber'); // Afiche "string"
+// console.log(typeof true); // Affiche "boolean"
+
+// var myVar;
+// console.log(typeof myVar); // Affiche "undefined"
+
+        // Afficher du texte
+
+// var age = 26;
+
+// document.write("<b>" + " Votre âge est : " + "</b>" + age);
+
+
+        // Method alert()
+
+// myVar = "Bonjour";
+// window.alert(myVar);
+// window.alert("chaîne de caractères");
+// window.alert(myVar + "chaîne de caractères");
+
+        // Method prompt()
+
+// var reponse1 = window.prompt("Saisissez votre nom");
+// var reponse2 = window.prompt("Saisissez votre couleur préférée");
+
+        // Method confirm()
+ 
+// if (window.confirm("Voulez-vous continuer ?") == true)
+// {
+//     windows.alert
+// }
+
+        // Method log
+ 
+// console.log("textea affiché dans la console");
+
+        // Exercice Afficher du texte
+
+// myVar = "Bonjour";
+// var bienvenue = "Bienvenue sur notre site web"
+// var reponse1 = window.prompt("Entrez votre nom");
+// var reponse2 = window.prompt("Entrez votre prénom")
+
+// if (window.confirm("Êtes-vous un homme ?") == true)
+// {
+//     window.alert( myVar + " " + reponse1 + " " + reponse2 + " " + "\n" + bienvenue);
+// }
+
