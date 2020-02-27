e12a4cb095f23cdee975b3a1529975cde30a92af
80010-09-04 <80010-09-04@exchange.ad.afpanet>
2020-02-27 15:35:53 +0100

    Fin de l'exercice 0 de JS06

--- Algo&Javascript/JS06.js
+++ Algo&Javascript/JS06.js
@@ -12,14 +12,15 @@
             // Conditions
 
 // var reponse == "oui"; 
-	
+
 // if (reponse == "oui")
 // {    
 //     console.log("Bonne réponse !");    // Affichera 'Bonne réponse' dans la console
 // }
 
-            // Conditions multiples
-// Conditions avec ET
+                // Conditions multiples
+        // Conditions avec ET
+
 // if (age > 18 && permis == "Voiture")
 // {
 //     console.log("Vous avez plus de 18 ans ET vous avez un permis e conduire voiture, vous pouvez conduire");
@@ -38,24 +39,93 @@
 
             // Conditions avec else
 
-var reponse = "oui";
+// var reponse = "oui";
 
-// // 1er cas
+    // 1er cas
 // if (reponse == "oui")
 // {
 //     console.log("Bonne réponse !");
 // }
 
 // 2ème cas
-if (reponse == "non")
-{
-    console.log("Mauvaise réponse !");
-}
+// if (reponse == "non")
+// {
+//     console.log("Mauvaise réponse !");
+// }
 
-if (reponse == "oui")
-{
-    console.log("Bonne réponse !");
-}
-else{
-    console.log("Mauvaise répose !");
-}
+// if (reponse == "oui")
+// {
+//     console.log("Bonne réponse !");
+// }
+// else{
+//     console.log("Mauvaise répose !");
+// }
+
+// var reponse = "B";
+
+// if (reponse == "A" )
+// {
+//     console.log("Bonne réponse");
+// }
+// else if (reponse =="B")
+// {
+//     console.log("Mauvaise réponse !");
+// }
+
+// var reponse = "C";
+
+// if (reponse == "A")
+// {
+//     console.log("Bonne réponse!");
+// }
+// else if (reponse == "B")
+// {
+//     console.log("Mauvaise réponse !");
+// }
+// else
+// {
+//     console.log("Réponse inconnue.");
+// }
+
+            // Instruction switch
+
+// var modele = "Clio";
+
+// switch (modele) {
+//     case "208":
+//         console.log("Modèle 208 : marque Peugeot");
+//         break;
+
+//     case "Clio":
+//         console.log("Modèle Clio : marque Renault");
+//         break;
+
+//     case "C3":
+//         console.log("Modèle C3 : marque Citroën");
+//         break;
+
+//     default:
+//         console.log("Modèle " + modele + ": marque inconnue");
+// }
+
+            // Imbrication de conditions
+
+// var reponse = "oui";
+
+// if (reponse == "oui")
+// {
+//     console.log("Bonne réponse!");
+
+//     score++; // augmente le score de 1
+
+//     if (score == 20)
+//     {
+//         console.log("Vous avez gagné !");
+//     }// fin du 2ème if
+// }//fin du 1er if
+
+
+                // Condition
+var age = 19;
+
+(age >=18) ? console.log("Vous êtes majeur") : console.log("Vous êtes mineur")
