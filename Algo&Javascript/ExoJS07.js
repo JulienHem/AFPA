8534021d5a43235fe4f1b66d2969d77138d73dc6
80010-09-04 <80010-09-04@exchange.ad.afpanet>
2020-03-02 14:15:02 +0100

    Fin de l'EXO de JS07

--- Algo&Javascript/ExoJS07.js
+++ Algo&Javascript/ExoJS07.js
@@ -84,6 +84,18 @@
 
                 // EXERCICE 1
 
-var prenom = window.prompt("Saisissez le prénom N°1");
+var i=1;
+var pre;
+console.log("Liste de prénoms");
+console.log("==================");
 
+while (pre != "" && i <= 10)
+{
+  pre=window.prompt("Saisissez le prénom" + " " +i)
+  result = pre
+  console.log("Prénom"+i+" = "+result);
 
+  i++
+} 
+
+console.log("Fin de la boucle");
