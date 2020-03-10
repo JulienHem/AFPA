94aef63a6dfc27238c7d078eb6aa4fb3d21974de
80010-09-04 <80010-09-04@exchange.ad.afpanet>
2020-03-10 16:56:00 +0100

    Fin de l'EXO 3 de JS09

---
+++ Algo&Javascript/JS09/Exo 3/Exo3.js
@@ -0,0 +1,24 @@
+function tri (tabl){
+    do {
+        changed = false;
+        for (i = 0; i < nb -1; i++){
+            if (tabl[i] > tabl[i + 1]){
+            tampon = tabl[i + 1]
+            tabl[i + 1] = tabl[i]
+            tabl[i] = tampon
+            changed = true;
+        }
+    }
+    }while(changed);
+}
+
+var tabl = new Array(parseInt(prompt("Saisissez le nombre de colonne : ")))
+var nb = tabl.length
+var tampon;
+for (i = 0; i < nb; i++){
+tabl[i] = parseInt(prompt("Saisissez vos numéros :" ))
+console.log(tabl)
+}
+
+tri(tabl)
+console.log(tabl)
