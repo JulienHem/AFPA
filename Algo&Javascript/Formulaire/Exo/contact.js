78d3e273e1731455a5746dc8f2cd1ffde8f84414
JulienHem <Fixen80@gmail.com>
2020-03-15 11:30:42 +0100

    Fin du formulaire JS

--- Algo&Javascript/Formulaire/Exo/contact.js
+++ Algo&Javascript/Formulaire/Exo/contact.js
@@ -1,14 +1,63 @@
 
 
 function verification() {
-var uncara = document.getElementsByClassName("uncara")
-if (uncara == "")
-    alert("Vous devez rentrer au moins 1 caractère")
+
+    // Permet de vérifier si il y'a au moins 1 caractère
+    var uncara = document.getElementById("societe").value
+    var filtre2 = new RegExp("^[a-z0-9A-Z -@]+$")
+    var resultat2 = filtre2.test(uncara)
+    if(resultat2 === false){
+        alert("Rentrez un nom de société correct")
+    }
+    else 
+    { }
+
+    var ville = document.getElementById("ville").value
+    var filtre3 = new RegExp("^[a-z0-9A-Z -@]+$")
+    var resultat3 = filtre3.test(ville)
+    if(resultat3 === false){
+        alert("Rentrez le nom de votre ville")
+    }
+    else 
+    { }
+
+    var societe = document.getElementById("contact").value
+    var filtre4 = new RegExp("^[a-z0-9A-Z -@]+$")
+    var resultat4 = filtre4.test(societe)
+    if(resultat4 === false){
+        alert("Entrez le nom de la personne à contacter")
+    }
+    else 
+    { }
+
+
+// Vérifie si il y'a 5 chiffres pour le code postal
+var cp = document.getElementById("cp").value;
+var filtre = new RegExp("^[0-9]{5}$");
+var resultat = filtre.test(cp);
+if (resultat === false){
+    alert("Entrez le code postal sur 5 chiffres")
+}
+else 
+{ }
+
+
+// Permet de vérifier si il y'a un "@" de rentrer pour le mail
+var arobase = document.getElementById("mail").value;
+var result = arobase.indexOf("@");
+console.log(arobase)
+    if (result == -1){
+    alert("Vous êtes obligé de rentrer un arobase!")
+    }
+    else 
+    { }
+
 }
 
 
-var verif = document.getElementById("button1");
-verif.addEventListener("click", function() {
+// Permet de lancer la verification quand on clique sur "Envoyer"
+var verif = document.getElementById("button1")
+verif.addEventListener("click", function () {
     verification()
 
 });
