b3a61077d3ccb65a688adb20c3c6db49b07da000
JulienHem <Fixen80@gmail.com>
2020-03-16 13:02:12 +0100

    Ajout de commentaire dans l'EXO 1 de l'évalutation

--- Algo&Javascript/Eval JS/Exercice 1/Exercice1.js
+++ Algo&Javascript/Eval JS/Exercice 1/Exercice1.js
@@ -1,12 +1,13 @@
+// Permet de rentrer les 2 valeurs et de les calculer dans une variable
 var pu = parseFloat(prompt("Saisir le prix de l'article"));
 var qte = parseFloat(prompt("Saisir la quantité commandée"));
 var tot = parseFloat(pu * qte);
 
-
+// La variable TOT va ici permettre de comparer sa valeur
 if (tot > 500){
     alert("Les frais de port sont gratuits !")
 }
-else {
+else { // Ce else va permettre de calculer les frais de port avec la condition du minimum de 6€
     alert("Vous avez 6€ de frais de port minimum");
     var port = parseFloat(tot * (2 / 100))
     alert("Le port est de " + port +"€")
@@ -17,7 +18,7 @@ else {
         }
 }
 
-if (tot >= 100 && tot <= 200){
+if (tot >= 100 && tot <= 200){ // Ici la condition permet de calculer la remise si le prix est entre 100 et 200 euros
     var r1 = parseFloat(tot * (1 - 5 / 100));
     var remise1 = alert("Vous avez une remise de 5%")
    
@@ -29,4 +30,5 @@ else {
 console.log(tot)
 console.log(somme)
 
+// Calcule du prix total avec la remise et les frais de port
 var somme = parseFloat(alert("Le prix total est de " + (r1 || r2 + port)));
