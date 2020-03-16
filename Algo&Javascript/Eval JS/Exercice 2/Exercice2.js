a44f9e814a3e3c41a22917027c010ad590ee1c9a
JulienHem <Fixen80@gmail.com>
2020-03-16 13:05:16 +0100

    Ajout de commentaires dans l'EXO 2 de l'eval JS

--- Algo&Javascript/Eval JS/Exercice 2/Exercice2.js
+++ Algo&Javascript/Eval JS/Exercice 2/Exercice2.js
@@ -1,10 +1,11 @@
+// On rentre les 2 valeurs qui nous donnerons la somme
 var n = parseInt(prompt("Insérez votre votre chiffre"));
 var c = parseInt(prompt("Insérez votre opérateur"))
 console.log("Somme des nombres inférieur à N");
 console.log("=========================");
 
 do {
-    var result = n + c
+    var result = n + c // Calcul de la somme de n et c
     console.log("Nombre " + n + "+" + c + "=" + result)
-    n--
-}while(n >= 0)
+    n-- // Décrémentation, ce qui permet d'aller du chiffre rentré jusqu'à 0
+}while(n >= 0) // Permet à N de ne pas aller en dessous de 0
