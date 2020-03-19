9c93574cbf8acbb19654bb4c4b27ff9809236959
JulienHem <Fixen80@gmail.com>
2020-03-19 10:09:52 +0100

    Modif dans EXO 6 et fin de l'EXO1/2 SQL

--- Algo&Javascript/Eval JS/Exercice 6/Exercice6.js
+++ Algo&Javascript/Eval JS/Exercice 6/Exercice6.js
@@ -3,7 +3,7 @@ var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "No
 
 do { // Cette boucle va permettre de rentrez un prénom et de le supprimer
     var prenom = prompt("Quel prénom recherchez-vous ?")
-    var tab = tab.filter(item => item !== prenom); // Va créer u nanutre tableau, moins la valeur entrée dans le prompt
+    var tab = tab.filter(item => item !== prenom); // Va créer un autre tableau, moins la valeur entrée dans le prompt
     tab.length = 10;
     console.log(tab)
 
--- SQL/exo1.sql
+++ SQL/exo1.sql
@@ -5,47 +5,50 @@ CREATE DATABASE Reservation;
 USE Reservation;
 
 CREATE TABLE Client (
-    adresse_client varchar (50) NULL,
-    nom_client varchar (50) NULL,
-    prenom_client varchar (50) NULL,
-    num_client INT AUTO_INCREMENT NOT NULL
-    ,CONSTRAINT client_PK PRIMARY KEY(num_client)
-);
-
-CREATE TABLE Reservation (
-    num_chambre INT NOT NULL AUTO_INCREMENT,
-    num_client INT NOT NULL,
-    date_debut INT NULL,
-    date_fin INT NULL,
-    date_reservation INT NULL,
-    montant_arrhes INT NULL,
-    prix_total INT NULL
-    ,CONSTRAINT Reservation_PK PRIMARY KEY(num_chambre, num_client)
-);
-
-CREATE TABLE Chambre (
-    capacite_chambre INT NULL,
-    degre_confort INT NULL,
-    exposition INT NULL,
-    type_chambre INT NULL,
-    num_chambre INT NOT NULL AUTO_INCREMENT ,
-    num_hotel INT NULL
-    ,CONSTRAINT Chambre_PK PRIMARY KEY(num_chambre)
-);
-
-CREATE TABLE Hotel (
-    capacite_hotel INT NULL,
-    categorie_hotel INT NULL,
-    nom_hotel varchar (50) NULL,
-    adresse_hotel varchar (50) NULL,
-    num_hotel INT NOT NULL AUTO_INCREMENT,
-    num_station INT NULL
-    ,CONSTRAINT Hotel_PK PRIMARY KEY(num_hotel)
-
+    cli_num_client INT AUTO_INCREMENT NOT NULL,
+    cli_adresse_client varchar (50) NULL,
+    cli_nom_client varchar (50) NULL,
+    cli_prenom_client varchar (50) NULL
+    ,CONSTRAINT Client_PK PRIMARY KEY (cli_num_client)
 );
 
 CREATE TABLE Station (
-    num_station INT NOT NULL AUTO_INCREMENT,
-    nom_station INT NULL
-    ,CONSTRAINT Station_PK PRIMARY KEY(num_station)
+    sta_num_station INT NOT NULL AUTO_INCREMENT,
+    sta_nom_station INT NULL
+    ,CONSTRAINT Station_PK PRIMARY KEY(sta_num_station)
+);
+
+CREATE TABLE Hotel (
+    hot_num_hotel INT NOT NULL AUTO_INCREMENT,
+    sta_num_station INT NULL,
+    hot_capacite_hotel INT NULL,
+    hot_categorie_hotel INT NULL,
+    hot_nom_hotel varchar (50) NULL,
+    hot_adresse_hotel varchar (50) NULL
+    ,CONSTRAINT Hotel_PK PRIMARY KEY(hot_num_hotel)
+    ,FOREIGN KEY (sta_num_station) REFERENCES Station(sta_num_station)
+);
+
+
+CREATE TABLE Chambre (
+    cha_num_chambre INT NOT NULL AUTO_INCREMENT,
+    hot_num_hotel INT NULL,
+    cha_capacite_chambre INT NULL,
+    cha_degre_confort INT NULL,
+    cha_exposition INT NULL,
+    cha_type_chambre INT NULL
+    ,CONSTRAINT Chambre_PK PRIMARY KEY(cha_num_chambre)
+    ,FOREIGN KEY (hot_num_hotel) REFERENCES Hotel(hot_num_hotel)
+);
+
+CREATE TABLE Reservation (
+    cha_num_chambre INT NOT NULL AUTO_INCREMENT,
+    cli_num_client INT NOT NULL,
+    res_date_debut INT NULL,
+    res_date_fin INT NULL,
+    res_date_reservation INT NULL,
+    res_montant_arrhes INT NULL,
+    res_prix_total INT NULL,
+    FOREIGN KEY (cli_num_client) REFERENCES Client(cli_num_client),
+    FOREIGN KEY (cha_num_chambre) REFERENCES Chambre(cha_num_chambre)
 );
