33add4c2e7564c03de5ed8354e580023d1255559
JulienHem <Fixen80@gmail.com>
2020-04-06 14:19:05 +0200

    Finalisation du formulaire, version finale

--- Evals/Evaluation JS/Exercice 7/js/Exercice7.js
+++ Evals/Evaluation JS/Exercice 7/js/Exercice7.js
@@ -8,25 +8,43 @@ function check(id, iderror, regex, errorMsg) { // Permet de centraliser les vari
     } else {
         idError.textContent = errorMsg;
         idError.style.color = "red";
+        console.log(id)
         return false;
     }
 }
 
-// function checkselect(id, iderror, errorMsg) {   TEMP
-//     var menuselect = document.getElementById(id).value;
-//     var iderrorSelect = document.getElementById(iderror);
-//     if (menuselect != "empty") {
-//         return true;
-//     } else {
-//         iderrorSelect.textContent = errorMsg;
-//         iderrorSelect.style.color = "red";
-//         return false;
-//     }
-// }
+function checkselect(id, iderror, errorMsg) {
+    var menuselect = document.getElementById("menu");
+    var iderrorSelect = menuselect.options[menuselect.selectedIndex].value;
+    var errorselect = document.getElementById("menu-error");
+    if (iderrorSelect != "empty") {
+        return true;
+    } else {
+        errorselect.textContent = errorMsg;
+        errorselect.style.color = "red";
+        return false;
+    }
+}
+
+function checkbox(id, iderror, errorMsg) {
+    var getcheckbox = document.getElementById("checkbox").checked;
+    var errorcheck = document.getElementById("check-error");
+    if (getcheckbox === true) {
+        return true;
+    }
+    else {
+        errorcheck.textContent = errorMsg;
+        errorcheck.style.color = "red";
+        return false;
+    }
+
+}
+
 
 function hide(iderror, errorMsg) { // Ici la fonction permet de cacher les messages d'erreurs si le champ est correct
     $("#" + iderror).hide(errorMsg)
 }
+
 
 var tabverif = [ // On rentre toutes les valeurs dans ce tableau pour les vérifier une à une
 
@@ -38,22 +56,35 @@ var tabverif = [ // On rentre toutes les valeurs dans ce tableau pour les vérif
     ["city", "ville-error", "^[0-9]*[a-zA-Z- éàèêûî]+$", "Veuillez rentrer une ville valable"],
     ["mail", "mail-error", "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", "Veuillez rentrer un mail valable"],
     ["question", "question-error", "^(.|\n){5,500}$", "Vous devez entrez au moins 5 caractères et vous ne pouvez dépasser 500 caractères"],
-    // ["menu", "menu-error", " ", "Vous devez sélectionner votre demande"],
+    ["menu", "menu-error", "Vous devez sélectionner votre demande"],
+    ["checkbox", "check-error", "Si vous n'acceptez pas le formulaire ne sera pas envoyé"],
 ]
 
 function verification(laverif) { // Appelle de la fonction pour commencer la vérification des données entrées dans les champs
 
-    if (laverif == "lol") { // Permet de différencier entre les erreurs lorqu'on clique "envoyer" et le temps réel
+    if (laverif == "verif") { // Permet de différencier entre les erreurs lorqu'on clique "envoyer" et le temps réel
 
         var sortie = true;
-        for (i = 0; i < tabverif.length; i++) { // Permet la vérification dans le tableau grâce à sa longueur
-            if (check(tabverif[i][0], tabverif[i][1], tabverif[i][2], tabverif[i][3]) == false)
-            {
-                sortie = false; // Si il y'a une erreur cela va les afficher
+        for (i = 0; i < tabverif.length - 2; i++) { // Permet la vérification dans le tableau grâce à sa longueur
+            if (check(tabverif[i][0], tabverif[i][1], tabverif[i][2], tabverif[i][3]) == false) {
+                sortie = false // Si il y'a une erreur cela va les afficher
+                console.log(sortie)
             }
+
+
+            if (checkselect(tabverif[8][0], tabverif[8][1], tabverif[8][2]) == false) {
+                sortie = false
+            }
+
+            if (checkbox(tabverif[9][0], tabverif[9][1], tabverif[9][2]) == false) {
+                sortie = false
+
+            }
+
         }
-        return sortie;
+        return sortie
     }
+
 }
 
 $("input, textarea, select").on("change", function () { // Permet le temps réel 
@@ -65,14 +96,11 @@ $("input, textarea, select").on("change", function () { // Permet le temps réel
     }
 });
 
-
-
-
 //  Lance la vérification au clique de "Envoyer" du formulaire
 var verif = document.getElementById("send")
 verif.addEventListener("click", function (event) {
     event.preventDefault()
-    if (verification("lol")) {
+    if (verification("verif")) {
         document.getElementById("formulaire").submit();
     }
 });
