6bebff5c6f5955375ae492386efa4c03a99cc830
JulienHem <Fixen80@gmail.com>
2020-06-30 07:07:42 +0200

    Changement CV

--- CDA/HTML/js/script.js
+++ CDA/HTML/js/script.js
@@ -1,16 +1,3 @@
-window.onscroll = function() {stickynav()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function stickynav(){
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-
 
 
 
