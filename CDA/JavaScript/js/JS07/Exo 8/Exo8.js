1cbdddb6ff2d8860c18ef58424f372fafcc68ec5
JulienHem <Fixen80@gmail.com>
2020-07-16 07:17:40 +0200

    Fin du JS

--- CDA/JavaScript/js/JS07/Exo 8/Exo8.js
+++ CDA/JavaScript/js/JS07/Exo 8/Exo8.js
@@ -1,10 +1,11 @@
-let mot = document.getElementById("mot").value;
-let nbVoyelle = 0;             // Je définis la valeur de nbVoyelle pour commencer à partir de 0
-let i = 0;
 
 bouton.addEventListener("click", function() {
+    let mot = document.getElementById("mot").value;
+    let nbVoyelle = 0;             // Je définis la valeur de nbVoyelle pour commencer à partir de 0
+    let i = 0;
 
-            // "for" me permet de donner toutes les conditions de "i"
+
+    // "for" me permet de donner toutes les conditions de "i"
 for (i = 0; i < mot.length; i++) {
             // Cela permet d'identifier les voyelles
     if ((mot[i]=="a")||(mot[i]=="e")||(mot[i]=="i")||(mot[i]=="o")||(mot[i]=="u")||(mot[i]=="y")) {
