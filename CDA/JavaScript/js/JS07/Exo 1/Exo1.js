78e9e5decc95df186ed488c1bb46701a134f4685
JulienHem <julien.hemmery@gmail.com>
2020-07-16 14:54:37 +0200

    Fin du JS

--- CDA/JavaScript/js/JS07/Exo 1/Exo1.js
+++ CDA/JavaScript/js/JS07/Exo 1/Exo1.js
@@ -1,16 +1,19 @@
 // // EXERCICE 1
 
+let i = 0;
 let result = "";
-let i =1;
 
 
 bouton.addEventListener("click", function() { // PERMET L'ENVOIE DE L'EXO AU CLIC
 let pre = document.getElementById("pren").value;
 
-result = i + " " + pre + " " + "\n" + result; // ON STOCK LES VALEURS POUR POUVOIR AFFICHER PLUSIEURS PRENOMS A L'ENVOI 
 
-if (pre != " ") { // PERMET AU SCRIPT DE S'ARRETER ET DE LANCER UNE ALERT AVEC TOUS LES PRENOMS SAISIS UNN FOIS LE CHAMP VIDE
+if (pre == "") { // PERMET AU SCRIPT DE S'ARRETER ET DE LANCER UNE ALERT AVEC TOUS LES PRENOMS SAISIS UNN FOIS LE CHAMP VIDE
   alert("Prénoms" + " = " + "\n" + result );
+
+}
+else {
+  result = i + " " + pre + " " + "\n" + result; // ON STOCK LES VALEURS POUR POUVOIR AFFICHER PLUSIEURS PRENOMS A L'ENVOI
   i++
 }
 
--- CDA/JavaScript/js/JS07/Exo 10/Exo10.js
+++ CDA/JavaScript/js/JS07/Exo 10/Exo10.js
@@ -1,21 +1,22 @@
 // EXERCICE 7
 
-var n = 0
-var result = true;
 
 bouton.addEventListener("click", function() { // PERMET L'ENVOIE DE L'EXO AU CLIC
-
-var nb = document.getElementById("nbre").value; // RECUPERE LA VALEUR
-
-for (n = 2; n < nb; n++) {
-  if (nb % n == 0)  // PERMET DEDIFFERENCIER SI UNE VALEUR EST PREMIER OU NON
+let message;
+let nb = document.getElementById("nbre").value; // RECUPERE LA VALEUR
+for (let n = 2; n < nb; n++) {
+  if (nb % n === 0)  // PERMET DEDIFFERENCIER SI UNE VALEUR EST PREMIER OU NON
   {
-    result = false;
-    alert(nb + " est pas premier ")
+   message = nb + " est pas premier ";
     break;
   }
-  else 
-  alert(nb + " est premier ")
-  break;
 }
+
+if (message) {
+    alert(message)
+}
+else {
+    alert(nb + " est premier");
+}
+
 });
